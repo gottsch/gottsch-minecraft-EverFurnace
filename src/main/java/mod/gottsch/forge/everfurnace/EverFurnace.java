672bfd59d82f0706c5c1183ce73a9f187ec137b6
gottsch <17928819+gottsch@users.noreply.github.com>
2025-03-12 07:58:39 -0400

    Revert "initial commit"
    This reverts commit aeb78f35583f96729a5c698f4e2d731035f04bd5.

--- src/main/java/mod/gottsch/forge/everfurnace/EverFurnace.java
+++ src/main/java/mod/gottsch/forge/everfurnace/EverFurnace.java
@@ -1,26 +1,9 @@
-/*
- * This file is part of EverFurnace.
- * Copyright (c) 2024 Mark Gottschling (gottsch)
- *
- * EverFurnace is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * EverFurnace is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with EverFurnace.  If not, see <http://www.gnu.org/licenses/lgpl>.
- */
 package mod.gottsch.forge.everfurnace;
 
 import net.minecraftforge.fml.common.Mod;
 
 /**
- * @author Mark Gottschling on 12/9/2024
+ * Created by Mark Gottschling on 12/9/2024
  */
 @Mod(value = EverFurnace.MOD_ID)
 public class EverFurnace {
